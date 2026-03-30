2f68533443a681e2fa9b2204c2bd3440eb71a298
origin <httpsgit@github.com:Mavericks34/helloworldd.git>
2026-03-30 10:38:11 +0300

    I completed all my homework assignments in one project

--- src/Main.java
+++ src/Main.java
@@ -50,6 +50,58 @@ public class Main {
         System.out.println("За месяц машина произвела " + resUlt * (a1month * 60) + " бутылок");
 
         System.out.println("Задание 5");
+        byte jar = 120;
+        byte white = 2;
+        byte brown = 4;
+        int result1 = 120/(2+4);
+        int am = result1 * white;
+        int bm = result1 * brown;
+        int ab = am + bm;
+        System.out.println("В школе,где " + result1 + " классов,нужно " + am + " банок белой краски и " + bm + " банок коричневой краски");
+
+        System.out.println("Задание 6");
+        short banana = 5 * 80;
+        short milk = 200 / 100 * 105;
+        short iceCream = 2 * 100;
+        short eggs = 4 * 70;
+        int result22 = banana + milk + iceCream + eggs;
+        double kg = result22 * 0.001;
+        System.out.println("Всего грамм " + result22 + " переведённых в кг " + kg);
+
+        System.out.println("Задание 7");
+        byte weight = 7;
+        double wei250 = 250 * 0.001;
+        System.out.println("Перевели в кг " + wei250 );
+        double wei500 = 500 * 0.001;
+        System.out.println("Перевели в кг " + wei500);
+        double wei1 = 7 / wei250;
+        System.out.println("Если терять по 250 грамм в день понадобиться " + wei1 + " дней");
+        double wei2 = 7 / wei500;
+        System.out.println("Если терять по 500 грамм в день понадобиться " + wei2 + " дней");
+        double wei1wei2 = (wei1 + wei2) / 2;
+        System.out.println("Может потребоваться в среднем дней,чтобы добиться результата " + wei1wei2);
+
+        System.out.println("Задание 8");
+        double maria = 67760 * 1.1;
+        double den = 83690 * 1.1;
+        double kris = 76230 * 1.1;
+        int maria12 = 67760 * 12;
+        int den12 = 83690 * 12;
+        int kris12 = 76230 * 12;
+        double maria112 = maria * 12;
+        double den112 = den * 12;
+        double kris112 = kris * 12;
+        System.out.println("Мария теперь получает " + maria + " рублей." + " Годовой доход вырос на " + (maria112 - maria12) + " рублей");
+        System.out.println("Денис теперь получает " + den + " рублей." + " Годовой доход вырос на " + (den112 - den12) + " рублей");
+        System.out.println("Кристина теперь получает " + kris + " рублейю." + " Годовой доход вырос на " + (kris112 - kris12) + " рублей");
+
+
+
+
+
+
+
+
         
 
 
